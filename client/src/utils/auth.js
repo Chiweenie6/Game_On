cdf008a950d9ed33ed3b7b721e9b5b46cc82ada7
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-04-18 18:26:07 -0700

    added mutations.js to utils, client side

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -5,11 +5,13 @@ class AuthService {
     return decode(this.getToken());
   }
 
+  // Check if user is logged in
   loggedIn() {
     const token = this.getToken();
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
+  // Check if the token is expired
   isTokenExpired(token) {
     const decoded = decode(token);
     if (decoded.exp < Date.now() / 1000) {
@@ -19,15 +21,18 @@ class AuthService {
     return false;
   }
 
+  // Gets token from local storage
   getToken() {
     return localStorage.getItem('id_token');
   }
 
+  // Saves the User's token to localStorage
   login(idToken) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
 
+  // Clears the User's token from the local storage
   logout() {
     localStorage.removeItem('id_token');
     window.location.reload();
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,45 +1,132 @@
 import { gql } from '@apollo/client';
 
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!, $email: String!, $password: String!) {
-    addProfile(name: $name, email: $email, password: $password) {
-      token
-      profile {
-        _id
-        name
-      }
-    }
-  }
-`;
-
-export const ADD_SKILL = gql`
-  mutation addSkill($profileId: ID!, $skill: String!) {
-    addSkill(profileId: $profileId, skill: $skill) {
-      _id
-      name
-      skills
-    }
-  }
-`;
-
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      profile {
+      user {
         _id
-        name
+        username
       }
     }
   }
 `;
 
-export const REMOVE_SKILL = gql`
-  mutation removeSkill($skill: String!) {
-    removeSkill(skill: $skill) {
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_Opinion = gql`
+  mutation addOpinion($opinionText: String!) {
+    addOpinion(opinionText: $opinionText) {
       _id
-      name
-      skills
+      opinionText
+      opinionAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const REMOVE_Opinion = gql`
+  mutation removeOpinion($opinionId: String!) {
+    removeOpinion(opinionId: $opinionId) {
+      _id
+      opinionText
+      opinionAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+export const ADD_COMMENT = gql`
+  mutation removeComment($opinionId: ID!, $commentId: String!) {
+    removeComment(opinionId: $opinionId, commentId: $commentId) {
+      _id
+      opinionText
+      opinionAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const REMOVE_COMMENT = gql`
+  mutation addComment($opinionId: ID!, $commentText: String!) {
+    addComment(opinionId: $opinionId, commentText: $commentText) {
+      _id
+      opinionText
+      opinionAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
+
+export const SAVE_GAME = gql`
+  mutation saveGame($input: GameInput) {
+    saveGame(input: $input) {
+      _id
+      username
+      email
+      gameCount
+      savedGames {
+        _id
+        title
+        image
+        genre
+        release
+        players
+        platform
+        publisher
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_GAME = gql`
+  mutation removeGame($gameId: String!) {
+    removeGame(gameId: $gameId) {
+      _id
+      username
+      email
+      gameCount
+      savedGames {
+        _id
+        title
+        image
+        genre
+        release
+        players
+        platform
+        publisher
+        description
+      }
     }
   }
 `;
